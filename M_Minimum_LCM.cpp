c547df5ce6e6a2c21a01c307ee98663790205b36
Harsh Gupta <harshgta13@gmail.com>
2026-06-03 22:18:50 +0530

    Solution

---
+++ M_Minimum_LCM.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+    #define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+    #define debug(x...)
+#endif
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+         int n;
+         cin>>n;
+         int a=1; //initialing value
+         int b=n-1; //initialing value
+         for(int i=2;i*i<=n;i++){ // 2, because 1 divides every number, √n, because √n x √n <= n
+            if(n%i==0){
+                a=n/i; // (n/i,n-n/i)pairs of factors
+                b=n-a;
+                break;
+            }
+         }
+         cout<<a<<" "<<b<<"\n";
+    }
+    return 0;
+}
